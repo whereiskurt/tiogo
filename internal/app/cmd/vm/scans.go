831fc2736268e8a3917498d42585eee096b4955d
Kurt Hundeck <whereiskurt@gmail.com>
2020-11-25 11:37:35 -0500

    -Reordered Scans -Added row filter and KeepAll

--- internal/app/cmd/vm/scans.go
+++ internal/app/cmd/vm/scans.go
@@ -13,53 +13,6 @@ import (
 	"github.com/whereiskurt/tiogo/pkg/ui"
 )
 
-// ScansList is invoked by Cobra with commandline args passed.
-func (vm *VM) ScansList(cmd *cobra.Command, args []string) {
-	logger := vm.setupLog()
-	cli := ui.NewCLI(vm.Config)
-
-	logger.Infof("Starting scan list ...")
-	a := client.NewAdapter(vm.Config, vm.Metrics)
-
-	maxkeep, err := strconv.Atoi(vm.Config.VM.MaxKeep)
-	if err != nil {
-		logger.Fatalf("error: couldn't convert maxkeep '%s': %v", vm.Config.VM.MaxKeep, err)
-	}
-
-	scans, err := a.Scans(true, true)
-	if err != nil {
-		logger.Fatalf("error: couldn't scans list: %v", err)
-	}
-	scans = vm.FilterScans(a, &scans)
-
-	if a.Config.VM.OutputJSON {
-		// Convert structs to JSON.
-		data, err := json.Marshal(scans)
-		if err != nil {
-			logger.Fatalf("error: couldn't marshal scan data to JSON: %v", err)
-		}
-		cli.Println(fmt.Sprintf("%s\n", data))
-
-	} else if a.Config.VM.OutputCSV || !a.Config.VM.OutputJSON {
-
-		dts := time.Now().Format("20060102T150405")
-		filename := fmt.Sprintf("scanlist.%s.csv", dts)
-		csv := cli.Render("ScansListCSV", map[string]interface{}{"Scans": scans})
-
-		// NOTE: Using ioutil.WriteFile is OK for smaller files (less than 100MBs)
-		err = ioutil.WriteFile(filename, []byte(csv), 0644)
-		if err != nil {
-			logger.Fatalf("can't write to file '%s': %+v", filename, err)
-		}
-
-		cleanTemplate := `scanlist\.\d+T\d+.csv`
-		logger.Infof("keeping a maximum '%d' for template '%s'", maxkeep, cleanTemplate)
-		vm.CleanupFiles(`.`, cleanTemplate, maxkeep)
-	}
-
-	return
-}
-
 // FilterScans uses cli arguments to reduce scans to filtered
 func (vm *VM) FilterScans(a *client.Adapter, scans *[]client.Scan) (filtered []client.Scan) {
 	//TODO: Make this a method :-P
@@ -97,72 +50,6 @@ func (vm *VM) FilterScans(a *client.Adapter, scans *[]client.Scan) (filtered []c
 
 		}
 		filtered = reduced
-	}
-
-	return
-}
-
-// ScansDetail is invoked by Cobra with commandline args passed.
-func (vm *VM) ScansDetail(cmd *cobra.Command, args []string) {
-	logger := vm.setupLog()
-	logger.Infof("tiogo scan detail command:")
-
-	cli := ui.NewCLI(vm.Config)
-
-	a := client.NewAdapter(vm.Config, vm.Metrics)
-
-	scans, err := a.Scans(true, true)
-	if err != nil {
-		logger.Errorf("error: couldn't scans list: %v", err)
-		return
-	}
-
-	id := vm.Config.VM.ID
-	uuid := vm.Config.VM.UUID
-	name := vm.Config.VM.Name
-	regex := vm.Config.VM.Regex
-	if id != "" {
-		scans = a.Filter.ScanByID(scans, id)
-	} else if uuid != "" {
-		scans = a.Filter.ScanByScheduleUUID(scans, uuid)
-	} else if name != "" {
-		scans = a.Filter.ScanByName(scans, name)
-	} else if regex != "" {
-		scans = a.Filter.ScanByRegex(scans, regex)
-	}
-
-	if len(scans) == 0 {
-		logger.Errorf("error: couldn't match a scans")
-		return
-	}
-
-	for i := range scans {
-		details, err := a.ScanDetails(&scans[i], true, true)
-		if err != nil {
-			logger.Fatalf("error: couldn't retrieve details: %v: %+v", scans[i], err)
-		}
-		cli.Println(fmt.Sprintf(" Name:\t\t\t%+v", details.Scan.Name))
-		cli.Println(fmt.Sprintf(" Enabled:\t\t%+v", details.Scan.Enabled))
-		cli.Println(fmt.Sprintf(" ScanID:\t\t%+v", details.Scan.ScanID))
-		cli.Println(fmt.Sprintf(" ScheduleUUID:\t\t%+v", details.Scan.ScheduleUUID))
-		cli.Println(fmt.Sprintf(" ScannerName:\t\t%+v", details.ScannerName))
-		cli.Println(fmt.Sprintf(" ScanType:\t\t%+v", details.ScanType))
-		cli.Println(fmt.Sprintf(" Target:\t\t%+v", details.Targets))
-		cli.Println(fmt.Sprintf(" AgentGroups:\t\t%+v", details.AgentGroup))
-		cli.Println(fmt.Sprintf(" HistoryCount:\t\t%+v", details.HistoryCount))
-		cli.Println(fmt.Sprintf(" Last Status:\t\t%+v", details.Status))
-		cli.Println(fmt.Sprintf(" Last StartTime:\t%+v", details.ScanStart))
-		cli.Println(fmt.Sprintf(" Last EndTime:\t\t%+v", details.ScanEnd))
-		cli.Println(fmt.Sprintf(" Timestamp:\t\t%+v", details.Timestamp))
-		cli.Println(fmt.Sprintf(" RRules:\t\t%+v", details.Scan.RRules))
-		cli.Println(fmt.Sprintf(" Hosts Scanned:\t\t%+v", details.HostCount))
-		cli.Println(fmt.Sprintf(" CriticalCount:\t\t%+v", details.PluginCriticalCount))
-		cli.Println(fmt.Sprintf(" HighCount:\t\t%+v", details.PluginHighCount))
-		cli.Println(fmt.Sprintf(" MediumCount:\t\t%+v", details.PluginMediumCount))
-		cli.Println(fmt.Sprintf(" LowCount:\t\t%+v", details.PluginLowCount))
-		cli.Println(fmt.Sprintf(" InfoCount:\t\t%+v", details.PluginInfoCount))
-		cli.Println(fmt.Sprintf(" ====================================\n\t\t\t%+v total", details.PluginTotalCount))
-		cli.Println(fmt.Sprintf(" \n--------------------------------------------\n"))
 	}
 
 	return
@@ -301,15 +188,131 @@ SCANS:
 
 			//vm.copyToFile(filename, tgtFilename)
 			logger.Infof("processing download with _time for %s uuid: %s", s.ScanID, export.FileUUID)
-			err = vm.TimestampCSVRows(filename, tgtFilename)
+
+			err = vm.ProcessCSVRow(filename, tgtFilename, vm.FilterKeepAll)
 			if err != nil {
 				os.Remove(tgtFilename)
 				logger.Fatalf("error: couldn't apply timestatmps write: %s to %s: %v, (out of disk space?)", filename, tgtFilename, err)
 			}
+
 		}
 
 		//Keep only X historicals for this ScanId
 		cleanTemplate := fmt.Sprintf(`scan\.%s\.history\.\d+\.csv`, s.ScanID)
+		logger.Infof("keeping a maximum '%d' for template '%s'", maxkeep, cleanTemplate)
+		vm.CleanupFiles(`.`, cleanTemplate, maxkeep)
+	}
+
+	return
+}
+
+// ScansDetail is invoked by Cobra with commandline args passed.
+func (vm *VM) ScansDetail(cmd *cobra.Command, args []string) {
+	logger := vm.setupLog()
+	logger.Infof("tiogo scan detail command:")
+
+	cli := ui.NewCLI(vm.Config)
+
+	a := client.NewAdapter(vm.Config, vm.Metrics)
+
+	maxkeep, err := strconv.Atoi(vm.Config.VM.MaxKeep)
+	if err != nil {
+		logger.Fatalf("error: couldn't convert maxkeep '%s': %v", vm.Config.VM.MaxKeep, err)
+	}
+
+	logger.Infof(("Getting scans from Tenable.io"))
+	scans, err := a.Scans(true, true)
+	if err != nil {
+		logger.Errorf("error: couldn't scans list: %v", err)
+		return
+	}
+
+	id := vm.Config.VM.ID
+	uuid := vm.Config.VM.UUID
+	name := vm.Config.VM.Name
+	regex := vm.Config.VM.Regex
+	if id != "" {
+		scans = a.Filter.ScanByID(scans, id)
+	} else if uuid != "" {
+		scans = a.Filter.ScanByScheduleUUID(scans, uuid)
+	} else if name != "" {
+		scans = a.Filter.ScanByName(scans, name)
+	} else if regex != "" {
+		scans = a.Filter.ScanByRegex(scans, regex)
+	}
+
+	if len(scans) == 0 {
+		logger.Errorf("error: couldn't match a scans")
+		return
+	}
+
+	dts := time.Now().Format("20060102T150405")
+	filename := fmt.Sprintf("scandetails.%s.csv", dts)
+
+	csv := fmt.Sprintf("%s\n", cli.Render("ScansDetailHeaderCSV", nil))
+	for i := range scans {
+		details, err := a.ScanDetails(&scans[i], true, true)
+		logger.Infof("Getting scan details for '%s' (%s) from Tenable.io", (&scans[i]).Name, (&scans[i]).ScanID)
+		if err != nil {
+			logger.Fatalf("error: couldn't retrieve details: %s: %+v", scans[i], err)
+		}
+		row := fmt.Sprintf("%s\n", cli.Render("ScansDetailCSV", map[string]interface{}{"d": details}))
+		csv += row
+	}
+
+	logger.Infof("Writing all results to file at once.")
+	err = ioutil.WriteFile(filename, []byte(csv), 0644)
+	if err != nil {
+		logger.Fatalf("can't write to file '%s': %+v", filename, err)
+	}
+
+	cleanTemplate := `scandetails\.\d+T\d+.csv`
+	logger.Infof("keeping a maximum '%d' for template '%s'", maxkeep, cleanTemplate)
+	vm.CleanupFiles(`.`, cleanTemplate, maxkeep)
+
+	return
+}
+
+// ScansList is invoked by Cobra with commandline args passed.
+func (vm *VM) ScansList(cmd *cobra.Command, args []string) {
+	logger := vm.setupLog()
+	cli := ui.NewCLI(vm.Config)
+
+	logger.Infof("Starting scan list ...")
+	a := client.NewAdapter(vm.Config, vm.Metrics)
+
+	maxkeep, err := strconv.Atoi(vm.Config.VM.MaxKeep)
+	if err != nil {
+		logger.Fatalf("error: couldn't convert maxkeep '%s': %v", vm.Config.VM.MaxKeep, err)
+	}
+
+	scans, err := a.Scans(true, true)
+	if err != nil {
+		logger.Fatalf("error: couldn't scans list: %v", err)
+	}
+	scans = vm.FilterScans(a, &scans)
+
+	if a.Config.VM.OutputJSON {
+		// Convert structs to JSON.
+		data, err := json.Marshal(scans)
+		if err != nil {
+			logger.Fatalf("error: couldn't marshal scan data to JSON: %v", err)
+		}
+		cli.Println(fmt.Sprintf("%s\n", data))
+
+	} else if a.Config.VM.OutputCSV || !a.Config.VM.OutputJSON {
+
+		dts := time.Now().Format("20060102T150405")
+		filename := fmt.Sprintf("scanlist.%s.csv", dts)
+		csv := cli.Render("ScansListCSV", map[string]interface{}{"Scans": scans})
+
+		// NOTE: Using ioutil.WriteFile is OK for smaller files (less than 100MBs)
+		err = ioutil.WriteFile(filename, []byte(csv), 0644)
+		if err != nil {
+			logger.Fatalf("can't write to file '%s': %+v", filename, err)
+		}
+
+		cleanTemplate := `scanlist\.\d+T\d+.csv`
 		logger.Infof("keeping a maximum '%d' for template '%s'", maxkeep, cleanTemplate)
 		vm.CleanupFiles(`.`, cleanTemplate, maxkeep)
 	}
